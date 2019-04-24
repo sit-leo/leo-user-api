b5f655807c7ff859900fa3b5566183b1719c0d5a
Keerati <keerati.jear@gmail.com>
2019-04-24 14:32:15 +0700

    [ADD] add find user by username query

--- src/main/java/app/leo/user/repositories/UserRepository.java
+++ src/main/java/app/leo/user/repositories/UserRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findById(long userId);
+    User findByUsername(String username);
 }
