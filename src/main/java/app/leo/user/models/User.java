6b0812956bc3d16035ca58c11cdcef3a2446ef18
Keerati <keerati.jear@gmail.com>
2019-04-24 13:33:08 +0700

    [ADD] add user model

---
+++ src/main/java/app/leo/user/models/User.java
@@ -0,0 +1,50 @@
+package app.leo.user.models;
+
+import javax.persistence.Entity;
+
+@Entity(name = "users")
+public class User {
+    private long id;
+    private String username;
+    private String password;
+    private String role;
+
+    public User(long id, String username, String password, String role) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
