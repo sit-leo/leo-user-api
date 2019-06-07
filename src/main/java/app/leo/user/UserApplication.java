c9ee7c538a30cc3e994c70765c5e358e0e6e9b00
Volku <Natchanon.minato@mail.kmutt.ac.th>
2019-06-08 02:11:40 +0900

    [UPDATE] add config for passwordEncoder

--- src/main/java/app/leo/user/UserApplication.java
+++ src/main/java/app/leo/user/UserApplication.java
@@ -2,10 +2,15 @@ package app.leo.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class UserApplication {
-
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(UserApplication.class, args);
 	}
