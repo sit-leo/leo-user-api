4e3f4248777d1b6d7014a2aab45eb524aab63f9d
Volku <Natchanon.minato@mail.kmutt.ac.th>
2019-06-08 02:15:10 +0900

    [ADD] add Username Duplicate Exception

---
+++ src/main/java/app/leo/user/exceptions/UsernameDuplicateException.java
@@ -0,0 +1,22 @@
+package app.leo.user.exceptions;
+
+public class UsernameDuplicateException extends HttpException {
+    public UsernameDuplicateException() {
+    }
+
+    public UsernameDuplicateException(String message) {
+        super(message);
+    }
+
+    public UsernameDuplicateException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public UsernameDuplicateException(Throwable cause) {
+        super(cause);
+    }
+
+    public UsernameDuplicateException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
