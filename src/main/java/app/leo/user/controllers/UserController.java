5551c594111539202b3235170f98eedd6b5fd2ed
Volku <Natchanon.minato@mail.kmutt.ac.th>
2019-06-08 02:14:23 +0900

    [ADD] create user for /user

--- src/main/java/app/leo/user/controllers/UserController.java
+++ src/main/java/app/leo/user/controllers/UserController.java
@@ -1,52 +1,30 @@
 package app.leo.user.controllers;
 
 
-import app.leo.user.DTO.Token;
-import app.leo.user.DTO.UserLoginRequest;
-import app.leo.user.exceptions.UserNotFoundException;
-import app.leo.user.exceptions.WrongPasswordException;
+import app.leo.user.DTO.UserDTO;
 import app.leo.user.models.User;
-import app.leo.user.services.TokenService;
 import app.leo.user.services.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
 
 @CrossOrigin("*")
 @RestController
 public class UserController {
 
     @Autowired
-    TokenService tokenService;
-
-    @Autowired
     UserService userService;
 
-    @PostMapping("/login")
-    public ResponseEntity<Token> login(@Valid @RequestBody UserLoginRequest userLoginRequest){
-        User user = this.userService.findByUsername(userLoginRequest.getUsername());
-        if(user == null){
-            throw new UserNotFoundException("User is not found");
-        }
-        boolean passwordIsCorrect = user.getPassword().equals(userLoginRequest.getPassword());
-        if(passwordIsCorrect){
-            return  new ResponseEntity<>(tokenService.generateTokenByUser(user), HttpStatus.CREATED);
-        }else{
-            throw new WrongPasswordException("Wrong username or password");
-        }
+    @PostMapping("/user")
+    public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO userDTO){
+        User user = userService.registerNewUserAccount(userDTO);
+        ModelMapper modelMapper = new ModelMapper();
+        UserDTO response = modelMapper.map(user,UserDTO.class);
+        return  new ResponseEntity<UserDTO>(response, HttpStatus.CREATED);
     }
 
-    @GetMapping("/me")
-    public ResponseEntity<User> getUser(
-            @RequestHeader(name = "Authorization", required = true) String token,
-            HttpServletRequest request
-    ) {
-        String username = tokenService.getUsernameFromToken(token);
-        User user = userService.findByUsername(username);
-        return new ResponseEntity<User>(user, HttpStatus.OK);
-    }
+
 }
