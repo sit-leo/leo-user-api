061f224f3efe62296230a312f1fbcfc38ea89e4b
Keerati <keerati.jear@gmail.com>
2019-08-16 13:21:03 +0700

    [REMOVE] remove unused code

--- src/main/java/app/leo/user/UserApplication.java
+++ src/main/java/app/leo/user/UserApplication.java
@@ -1,32 +1,19 @@
 package app.leo.user;
 
-import app.leo.user.models.RefreshToken;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 
 @SpringBootApplication
 public class UserApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(UserApplication.class, args);
+	}
+
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
-	}
-	@Bean
-	public TokenStore tokenStore(RedisConnectionFactory redisConnectionFactory) {
-		return new RedisTokenStore(redisConnectionFactory);
-	}
-
-
-	public static void main(String[] args) {
-		SpringApplication.run(UserApplication.class, args);
 	}
 
 }
