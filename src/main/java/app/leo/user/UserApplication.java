b2516f79df82657066228c3976c7d918b7bd0c06
REi <Natchanon.minato@mail.kmutt.ac.th>
2019-08-19 13:11:53 +0700

    [ADD] add model mapper bean

--- src/main/java/app/leo/user/UserApplication.java
+++ src/main/java/app/leo/user/UserApplication.java
@@ -1,5 +1,6 @@
 package app.leo.user;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -16,4 +17,8 @@ public class UserApplication {
 		return new BCryptPasswordEncoder();
 	}
 
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
 }
